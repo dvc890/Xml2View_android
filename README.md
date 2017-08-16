a8e0e58aa617435e6bf959b6f746f780d57de782
dvc890 <dvc890@139.com>
2017-08-16 10:59:51 +0800

    support include layout

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 支持自定义View
 支持Assets下的资源id
+支持include字段
--- xmlconvert/src/main/java/com/dvc/xml/XmlDynamicView.java
+++ xmlconvert/src/main/java/com/dvc/xml/XmlDynamicView.java
@@ -1,5 +1,16 @@
 package com.dvc.xml;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
 import android.content.Context;
 import android.text.TextUtils;
 import android.util.AttributeSet;
@@ -8,57 +19,50 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-import org.xmlpull.v1.XmlPullParserFactory;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
 /**
  * Created by dvc on 17/7/2017.
  * parse the json as a tree and create View with its dynamicProperties
  */
 public class XmlDynamicView {
 
+    private static final String TAG_MERGE = "merge";
+    private static final String TAG_INCLUDE = "include";
+    private static final String TAG_1995 = "blink";
+    private static final String TAG_REQUEST_FOCUS = "requestFocus";
+    private static final String TAG_TAG = "tag";
     static int mCurrentId = 13;
     static int INTERNAL_TAG_ID = 0x7f020000;
     static final Class<?>[] mConstructorSignature = new Class[] {
-        Context.class, AttributeSet.class};
-	private static boolean DEBUG = true;
-	
-	public static View createView (Context context, String xmlPath, Class<?> holderClass) {
-			XmlPullParser xmlPullParser = null;
-			try {
-		        XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-		        xmlPullParser = factory.newPullParser();
-		        factory.setValidating(true);
-	            InputStream is = context.getAssets().open(xmlPath);
-	            try {
-	            	xmlPullParser.setInput(is, "utf-8");
-	                is.close();
-	            } catch (XmlPullParserException e) {
-	                // TODO Auto-generated catch block
-	                e.printStackTrace();
-	            }
+            Context.class, AttributeSet.class};
+    private static boolean DEBUG = true;
 
-	        } catch (IOException e) {
-	            // TODO Auto-generated catch block
-	            e.printStackTrace();
-	        } catch (XmlPullParserException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
+    public static View createView (Context context, String xmlPath, Class<?> holderClass) {
+        XmlPullParser xmlPullParser = null;
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            xmlPullParser = factory.newPullParser();
+            factory.setValidating(true);
+            InputStream is = context.getAssets().open(xmlPath);
+            try {
+                xmlPullParser.setInput(is, "utf-8");
+            } catch (XmlPullParserException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
 
-	        return createView(context, xmlPullParser, null, holderClass);
-	}
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (XmlPullParserException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+
+        return createView(context, xmlPullParser, null, holderClass);
+    }
 
     /**
-     * @param XmlPullParser : xml PullParser
+     * @param xmlPullParser : xml PullParser
      * @param holderClass : class that will be created as an holder and attached as a tag in the View
      * @return the view that created
      */
@@ -68,29 +72,29 @@ public class XmlDynamicView {
 
 
     /**
-     * @param XmlPullParser : xml PullParser
+     * @param xmlPullParser : xml PullParser
      * @param parent : parent viewGroup
      * @param holderClass : class that will be created as an holder and attached as a tag in the View, If contains HashMap ids will replaced with idsMap
      * @return the view that created
      */
     public static View createView (Context context, XmlPullParser xmlPullParser, ViewGroup parent, Class<?> holderClass) {
-    	
-    	if(xmlPullParser == null )
-    		return null;
+
+        if(xmlPullParser == null )
+            return null;
         HashMap<String, Integer> ids = new HashMap<>();
 
-		try {
-	        if (xmlPullParser.next() != XmlPullParser.START_TAG) {
-	            throw new InflateException(xmlPullParser.getPositionDescription()
-	                    + ": No start tag found!");
-	        }
-		} catch (XmlPullParserException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        try {
+            if (xmlPullParser.next() != XmlPullParser.START_TAG) {
+                throw new InflateException(xmlPullParser.getPositionDescription()
+                        + ": No start tag found!");
+            }
+        } catch (XmlPullParserException | IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         if(parent == null){
-        	parent = new FrameLayout(context);
-        	parent.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            parent = new FrameLayout(context);
+            parent.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
         }
         View container = createViewInternal(context, xmlPullParser, parent, ids);
 
@@ -120,14 +124,61 @@ public class XmlDynamicView {
             }
 
         }
-    	parent.addView(container);
+        parent.addView(container);
         return parent;
     }
-    
+
+    /**
+     * @param layoutid : include layoutid
+     * @param parent : parent viewGroup
+     * @param ids : the hashMap where we keep ids as string from json to ids as int in the layout
+     * @return the includelayout that created
+     */
+    public static View createIncludeView (Context context, String layoutid, ViewGroup parent, HashMap<String, Integer> ids) {
+        XmlPullParser xmlPullParser = null;
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            xmlPullParser = factory.newPullParser();
+            factory.setValidating(true);
+            InputStream is = context.getAssets().open(layoutid.substring(1)+".xml");
+            try {
+                xmlPullParser.setInput(is, "utf-8");
+            } catch (XmlPullParserException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (XmlPullParserException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        if(xmlPullParser == null )
+            return null;
+
+        try {
+            if (xmlPullParser.next() != XmlPullParser.START_TAG) {
+                throw new InflateException(xmlPullParser.getPositionDescription()
+                        + ": No start tag found!");
+            }
+        } catch (XmlPullParserException | IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        View container = createViewInternal(context, xmlPullParser, parent, ids);
+
+        if (container==null)
+            return null;
+
+        return container;
+    }
+
 
     /**
      * use internal to parse the json as a tree to create View
-     * @param XmlPullParser : xml PullParser
+     * @param xmlPullParser : xml PullParser
      * @param ids : the hashMap where we keep ids as string from json to ids as int in the layout
      * @return the view that created
      */
@@ -140,70 +191,86 @@ public class XmlDynamicView {
         int type;
         String widget = xmlPullParser.getName();
         try {
-	
-	        
-	        if (DEBUG ) {
-	            System.out.println("**************************");
-	            System.out.println("Creating root view: "
-	                    + widget);
-	            System.out.println("**************************");
-	        }
-            if (!xmlPullParser.getName().contains(".")) {
-                widget = "android.widget." + xmlPullParser.getName();
+
+
+            if (DEBUG ) {
+                System.out.println("**************************");
+                System.out.println("Creating root view: "
+                        + widget);
+                System.out.println("**************************");
             }
-            try{
-            	Class<?> viewClass = Class.forName(widget);
-                view = (View) viewClass.getConstructor(Context.class).newInstance(new Object[] { context });
-            }catch (ClassNotFoundException e) {
+            if (TAG_INCLUDE.equals(widget)) {
+                if (xmlPullParser.getDepth() == 0) {
+                    throw new InflateException("<include /> cannot be the root element");
+                }
+                String layoutid = "";
+                for(int i = 0; i < xmlPullParser.getAttributeCount(); i++){
+                    String xmlAttributeName = xmlPullParser.getAttributeName(i);
+                    if(xmlPullParser.getAttributeName(i).equals("layout")){
+                        layoutid = xmlPullParser.getAttributeValue(i);
+                        break;
+                    }
+                }
+                if(layoutid.length()>0)
+                    return createIncludeView(context, layoutid, parent, ids);
+            } else{
                 if (!xmlPullParser.getName().contains(".")) {
-                    widget = "android.view." + xmlPullParser.getName();
+                    widget = "android.widget." + xmlPullParser.getName();
                 }
                 try{
-                	Class<?> viewClass = Class.forName(widget);
+                    Class<?> viewClass = Class.forName(widget);
                     view = (View) viewClass.getConstructor(Context.class).newInstance(new Object[] { context });
-                }catch (ClassNotFoundException e1) {
-	    			// TODO Auto-generated catch block
-	    			e1.printStackTrace();
+                }catch (ClassNotFoundException e) {
+                    if (!xmlPullParser.getName().contains(".")) {
+                        widget = "android.view." + xmlPullParser.getName();
+                    }
+                    try{
+                        Class<?> viewClass = Class.forName(widget);
+                        view = (View) viewClass.getConstructor(Context.class).newInstance(new Object[] { context });
+                    }catch (ClassNotFoundException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
+                    }
                 }
-    		} 
+            }
 
-		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoSuchMethodException e) {
-        	Class<?> viewClass;
-			try {
-				viewClass = Class.forName(widget);
-	            view = (View) viewClass.getConstructor(mConstructorSignature).newInstance(new Object[] { context, null });
-			} catch (ClassNotFoundException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			} catch (InstantiationException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			} catch (IllegalAccessException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			} catch (IllegalArgumentException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			} catch (InvocationTargetException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			} catch (NoSuchMethodException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-		}
+        } catch (InstantiationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            Class<?> viewClass;
+            try {
+                viewClass = Class.forName(widget);
+                view = (View) viewClass.getConstructor(mConstructorSignature).newInstance(new Object[] { context, null });
+            } catch (ClassNotFoundException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            } catch (InstantiationException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            } catch (IllegalAccessException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            } catch (IllegalArgumentException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            } catch (InvocationTargetException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            } catch (NoSuchMethodException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+        }
         if (view==null) return null;
 
         /* default Layout in case the user not set it */
@@ -236,24 +303,24 @@ public class XmlDynamicView {
             /* parse the aray to get the children views */
             List<View> views = new ArrayList<>();
             try {
-    	        final int depth = xmlPullParser.getDepth();
-    	        while (((type = xmlPullParser.next()) != XmlPullParser.END_TAG ||
-    	        		xmlPullParser.getDepth() > depth) && type != XmlPullParser.END_DOCUMENT) {
+                final int depth = xmlPullParser.getDepth();
+                while (((type = xmlPullParser.next()) != XmlPullParser.END_TAG ||
+                        xmlPullParser.getDepth() > depth) && type != XmlPullParser.END_DOCUMENT) {
 
-    	            if (type != XmlPullParser.START_TAG) {
-    	                continue;
-    	            }
+                    if (type != XmlPullParser.START_TAG) {
+                        continue;
+                    }
                     /* create every child add it in viewGroup and set its tag with its properties */
-                    View dynamicChildView = createViewInternal(context, xmlPullParser, parent, ids);
+                    View dynamicChildView = XmlDynamicView.createViewInternal(context, xmlPullParser, parent, ids);
                     if (dynamicChildView!=null) {
                         views.add(dynamicChildView);
                         viewGroup.addView(dynamicChildView);
                     }
                 }
-			} catch (XmlPullParserException | IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+            } catch (XmlPullParserException | IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
             /* after create all the children apply layout properties
             * we need to do this after al children creation to have create all possible ids */
             for(View v : views) {
